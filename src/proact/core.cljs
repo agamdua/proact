cfcd6b18373add26edececa3b989bdd8e8608dd9
Vector919 <manbitesdog326@gmail.com>
2015-06-04 21:00:14 -0400

    added kirtify and replace functions to parse template syntax

--- src/proact/core.cljs
+++ src/proact/core.cljs
@@ -1,8 +1,17 @@
 (ns proact.core
   (:require [clojure.browser.repl :as repl]))
-
 ;; (repl/connect "http://localhost:9000/repl")
 
-(enable-console-print!)
+(defn replace_match [match context]
+    ;; eval a match and then if it works return it
+    (def res (.replace match (get (keyword (.split match #"\.") ) context)))
+    res
+)
 
-(println "Hello world!")
+(defn kirtify [vars context]
+    (def parse_vars (js/RegExp "{{([^}}]+)}}"))
+    (def matches (.exec parse_vars vars))
+    (def line (nth matches 1))
+    (def final_vars (replace_match line context))
+    final_vars
+)

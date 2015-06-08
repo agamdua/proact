269111210a266a9988b5d22aec34acbb44cad56b
Vector919 <manbitesdog326@gmail.com>
2015-06-07 20:24:36 -0400

    review comments #1: changed function names

--- src/proact/core.cljs
+++ src/proact/core.cljs
@@ -1,17 +1,17 @@
 (ns proact.core
   (:require [clojure.browser.repl :as repl]))
-;; (repl/connect "http://localhost:9000/repl")
 
-(defn replace_match [match context]
+(defn template-parse [match context]
     ;; eval a match and then if it works return it
     (def res (.replace match (get (keyword (.split match #"\.") ) context)))
     res
 )
 
-(defn kirtify [vars context]
+(defn regex-replace [vars context]
+    ;; exec the regex on a template string and then replace the results
     (def parse_vars (js/RegExp "{{([^}}]+)}}"))
     (def matches (.exec parse_vars vars))
     (def line (nth matches 1))
-    (def final_vars (replace_match line context))
+    (def final_vars (template-parse line context))
     final_vars
 )
